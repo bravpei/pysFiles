269873fcb6c53779ee79f940acdcbfc566acb87e
liupei-gs <403664797@qq.com>
2018-06-13 09:43:41 +0800

    2

--- component/ProcessDriver.py
+++ component/ProcessDriver.py
@@ -1,10 +1,14 @@
 import sys
 sys.path.append(r"/opt/test/component/ProcessEngine")
 from component.ProcessEngine import ProcessEngine
+from component.ProcessEngineSQL import ProcessEngineSQL
 class ProcessDriver():
     def __init__(self,appName,processConfig):
         self.__appName=appName
         self.__processConfig=processConfig
-    def start(self):
+    def startCore(self):
         pe=ProcessEngine(self.__appName)
+        pe.execute(self.__processConfig)
+    def startSQL(self):
+        pe=ProcessEngineSQL(self.__appName)
         pe.execute(self.__processConfig)
